57ae738e15b5e6e4afbfb2d9f956187666e56cd3
Emmanuel Adeola <emmanueladeola990@gmail.com>
2022-11-28 15:08:28 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//dotnet is used in running things here e.g dotnet run ... So you can find out more about it
+//Console.WriteLine("Hello, World!");
+using System;
+namespace VSCodeNetCoreTest
+{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello world!");
+        }
+    }
+    
+}
